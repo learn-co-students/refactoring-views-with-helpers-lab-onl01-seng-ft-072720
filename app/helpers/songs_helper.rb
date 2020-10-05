ed0b199556d0e09297be6e30631bd5ca12d04ae5
KerriLynne <kerri.l.ogorman@gmail.com>
2020-10-05 11:40:54 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,17 @@
 module SongsHelper
 
+    def display_artist(song)
+        #  should respond to #display_artist with 1 argument
+        if song.artist.nil?
+            link_to 'Add Artist', edit_song_path(song)
+            # displays a link to edit the song if artist empty
+        else
+            link_to song.artist_name, artist_path(song.artist)
+            # displays a link to the artist page if artist not empty
+        end
+    end
 end
+
+
+
+
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name unless artist.nil?
+    #  gets the artist name
   end
 
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+    # can set the artist via name
+    # finds artist if already exists
   end
 end
